2309b5292e64cad7e72d7876ea41994c603b283c
GodmodeONf <116742822+GodmodeONf@users.noreply.github.com>
2025-06-02 23:50:14 +0400

    Update BR-SRV.sh

--- BR-SRV.sh
+++ BR-SRV.sh
@@ -2,28 +2,23 @@ apt-get install -y tzdata
 hostnamectl set-hostname br-srv.au-team.irpo;
 timedatectl set-timezone Europe/Samara
 
-cat <<EOF > /etc/net/ifaces/ens18/options
+cd /etc/net/ifaces
+cd ens18
+vim options
+BOOTPROTO=static
 TYPE=eth
+CONFIG_WIRELESS=no
+SYSTEMD_BOOTPROTO=static
+CONFIG_IPV4=yes
 DISABLED=no
 NM_CONTROLLED=no
-BOOTPROTO=static
-IPV4_CONFIG=yes
-EOF
-
-touch /etc/net/ifaces/ens18/ipv4address
-cat <<EOF > /etc/net/ifaces/ens18/ipv4address
-192.168.24.30/27
-EOF
-
-touch /etc/net/ifaces/ens18/ipv4route
-cat <<EOF > /etc/net/ifaces/ens18/ipv4route
-default via 192.168.24.1
-EOF
-
-cat <<EOF > /etc/resolv.conf
-nameserver 8.8.8.8
-nameserver 77.88.8.8
-EOF
+SYSTEMD_CONTROLLED=no
+echo "192.168.24.30/27" > /etc/net/ifaces/ens18/ipv4address
+echo "default via 192.168.24.1" > /etc/net/ifaces/ens18/ipv4route
+echo nameserver 8.8.8.8 > /etc/resolv.conf
+ip -c -br a
+ip route show
+ip a show
 
 timedatectl set-timezone Europe/Samara
 systemctl restart network
