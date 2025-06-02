bbd5d0abd5e09f2dc73b45dd4fa2729c38ecf593
GodmodeONf <116742822+GodmodeONf@users.noreply.github.com>
2025-06-02 23:19:58 +0400

    Update ISP.sh

--- ISP.sh
+++ ISP.sh
@@ -2,47 +2,30 @@ apt-get install -y tzdata
 hostnamectl set-hostname isp; exec bash
 timedatectl set-timezone Europe/Samara
 
-mkdir /etc/net/ifaces/ens19
-mkdir /etc/net/ifaces/ens20
-
-touch /etc/net/ifaces/ens19/options
-touch /etc/net/ifaces/ens20/options
-
-cat <<EOF > /etc/net/ifaces/ens19/options
-TYPE=eth
-DISABLED=no
-NM_CONTROLLED=no
+cd /etc/net/ifaces/
+cp -r ens18/ ens19
+cd ens19
+vim options
 BOOTPROTO=static
-IPV4_CONFIG=yes
-EOF
-
-cat <<EOF > /etc/net/ifaces/ens20/options
-TYPE=eth
+TIPE=eth
 DISABLED=no
-NM_CONTROLLED=no
-BOOTPROTO=static
-IPV4_CONFIG=yes
-EOF
-
-cat <<EOF > /etc/net/ifaces/ens19/ipv4address
-172.16.4.1/28
-EOF
-
-cat <<EOF > /etc/net/ifaces/ens20/ipv4address
-172.16.5.1/28
-EOF
-
-sed -i 's/net.ipv4.ip_forward = 0/net.ipv4.ip_forward = 1/g' /etc/net/sysctl.conf
-на этом завершается 1 модуль
-apt-get update && apt-get install -y firewalld 
-systemctl enable --now firewalld
-firewall-cmd --permanent --zone=public --add-interface=ens18
-firewall-cmd --permanent --zone=trusted --add-interface=ens19
-firewall-cmd --permanent --zone=trusted --add-interface=ens20
-firewall-cmd --permanent --zone=public --add-masquerade
-firewall-cmd --reload
-systemctl restart firewalld
+ONBOOT=yes
+cd ens19
+echo "172.16.4.1/28 >> ipv4address
+cd..
+cp -r ens19/ ens20
+echo "172.16.5.1/28" > ens20/ipv4address
 systemctl restart network
+ip -c br a
+vim /etc/net/sysctl.conf
+net.ipv4.ip_forward = 1
+sysctl -w net.ipv4.ip_forward=1
+apt-get update && apt-get install -y iptables
+ipbables -A POSTROUTING -t nat -s 172.16.4.0/28 -o ens18 -j MASQUERADE
+ipbables -A POSTROUTING -t nat -s 172.16.5.0/28 -o ens18 -j MASQUERADE
+iptables -t nat -L
+iptables-save > /etc/sysconfig/iptables
+systemctl enable --now iptables.service
 
 useradd sshuser -u 1010
 echo "sshuser:P@ssw0rd" | chpasswd
